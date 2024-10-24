0fb9a74c8671239106bacbf6803858894d59196f
Maksym Korniyko <korniykom@gmail.com>
2024-10-24 20:20:27 +0300

    hotfix: update FuncA to return sum of first 3 elements

--- TrigFunction.cpp
+++ TrigFunction.cpp
@@ -1,5 +1,7 @@
 #include "TrigFunction.h"
+#include <cmath> 
 
 double TrigFunction::FuncA(int n) {
-    return 0; // Initial implementation
+    if (n < 1) return 0.0; 
+    return 1.0 + (1.0/3.0) + (2.0/15.0);
 }
