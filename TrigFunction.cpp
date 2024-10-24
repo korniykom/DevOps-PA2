e223be4d4778f8d386743e47bc0ffe22f14ea35c
Maksym Korniyko <korniykom@gmail.com>
2024-10-24 20:24:35 +0300

    feat: add full implementation of FuncA

--- TrigFunction.cpp
+++ TrigFunction.cpp
@@ -1,5 +1,12 @@
 #include "TrigFunction.h"
+#include <cmath> 
 
 double TrigFunction::FuncA(int n) {
-    return 0; // Initial implementation
+    double sum = 0.0;
+    for (int i = 1; i <= n; ++i) {
+        double term = (i % 2 == 0 ? -1 : 1) * (pow(4, i - 1) * (1 - pow(4, i))) / (2 * i);
+        sum += term;
+    }
+    
+    return sum; 
 }
